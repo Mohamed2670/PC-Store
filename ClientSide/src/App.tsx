3de4ba8ada12a8dfa0fc4bb792bb2b325d0e5a86
MedoZzZ <mh639450@gmail.com>
2025-01-21 20:58:21 +0200

    Initial commit

---
+++ ClientSide/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import LogIn from './Components/Login'
+
+function App() {
+  return (
+    <>
+      <LogIn/>
+    </>
+  )
+}
+
+export default App
---
+++ ClientSide/src/Components/LogIn.tsx
@@ -0,0 +1,11 @@
+function LogIn() {
+  return (
+    <>
+          <div>
+              <h1>Login page</h1>
+      </div>
+    </>
+  );
+}
+
+export default LogIn;
